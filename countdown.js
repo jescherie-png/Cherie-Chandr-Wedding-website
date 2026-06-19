1e195dadd56beee32fffb5270b89cb0b9f2181b4
jescherie-png <jescherie@gmail.com>
2026-06-19 09:03:34 +0200

    Add alert for countdown JS loading

--- countdown.js
+++ countdown.js
@@ -1,13 +1,1 @@
-const weddingDate = new Date("June 5, 2027 14:00:00").getTime();
-
-setInterval(function() {
-
-    const now = new Date().getTime();
-    const distance = weddingDate - now;
-
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-
-    document.getElementById("countdown").innerHTML =
-        "💜 " + days + " days until our wedding";
-
-}, 1000);
+alert("Countdown JS Loaded");
