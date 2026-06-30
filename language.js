a1aa21314949b78bffeeaebd9bba88462db44a1c
jescherie-png <jescherie@gmail.com>
2026-06-30 09:51:57 +0200

    Add multiple guest entries to language.js

--- language.js
+++ language.js
@@ -143,6 +143,146 @@ const guestList = [
         familyGroup: "Lesley Mills",
         mainContact: true
     },
+       {
+        id: 21,
+        firstName: "Zaira",
+        surname: "Van Wyk",
+        familyGroup: "Zaira Van Wyk",
+        mainContact: true
+    },
+    {
+        id: 22,
+        firstName: "Piston",
+        surname: "Van Wyk",
+        familyGroup: "Zaira Van Wyk",
+        mainContact: false
+    },
+    {
+        id: 23,
+        firstName: "Mariska",
+        surname: "Lubbe",
+        familyGroup: "Mariska Lubbe",
+        mainContact: true
+    },
+    {
+        id: 24,
+        firstName: "Nandre",
+        surname: "Lubbe",
+        familyGroup: "Mariska Lubbe",
+        mainContact: false
+    },
+    {
+        id: 25,
+        firstName: "Ria",
+        surname: "Allen",
+        familyGroup: "Ria Allen",
+        mainContact: true
+    },
+    {
+        id: 26,
+        firstName: "Gert",
+        surname: "Allen",
+        familyGroup: "Ria Allen",
+        mainContact: false
+    },
+    {
+        id: 27,
+        firstName: "Elize",
+        surname: "Combrink",
+        familyGroup: "Elize Combrink",
+        mainContact: true
+    },
+    {
+        id: 28,
+        firstName: "André",
+        surname: "Combrink",
+        familyGroup: "Elize Combrink",
+        mainContact: false
+    },
+    {
+        id: 29,
+        firstName: "Marelize",
+        surname: "Combrink",
+        familyGroup: "Elize Combrink",
+        mainContact: false
+    },
+    {
+        id: 30,
+        firstName: "Carina",
+        surname: "Naudé",
+        familyGroup: "Carina Naudé",
+        mainContact: true
+    },
+    {
+        id: 31,
+        firstName: "Japie",
+        surname: "Naudé",
+        familyGroup: "Carina Naudé",
+        mainContact: false
+    },
+    {
+        id: 32,
+        firstName: "Milandri",
+        surname: "Botes",
+        familyGroup: "Milandri Botes",
+        mainContact: true
+    },
+    {
+        id: 33,
+        firstName: "Marcel",
+        surname: "Botes",
+        familyGroup: "Milandri Botes",
+        mainContact: false
+    },
+    {
+        id: 34,
+        firstName: "Surita",
+        surname: "Botes",
+        familyGroup: "Surita Botes",
+        mainContact: true
+    },
+    {
+        id: 35,
+        firstName: "Corné",
+        surname: "Botes",
+        familyGroup: "Surita Botes",
+        mainContact: false
+    },
+    {
+        id: 36,
+        firstName: "David",
+        surname: "Grobbelaar",
+        familyGroup: "David Grobbelaar Jnr",
+        mainContact: true
+    },
+    {
+        id: 37,
+        firstName: "Amelia",
+        surname: "Grobbelaar",
+        familyGroup: "David Grobbelaar Jnr",
+        mainContact: false
+    },
+    {
+        id: 38,
+        firstName: "Dewald",
+        surname: "Grobbelaar",
+        familyGroup: "David Grobbelaar Jnr",
+        mainContact: false
+    },
+    {
+        id: 39,
+        firstName: "Hester",
+        surname: "Smal",
+        familyGroup: "Hester Smal",
+        mainContact: true
+    },
+    {
+        id: 40,
+        firstName: "Jan",
+        surname: "Smal",
+        familyGroup: "Hester Smal",
+        mainContact: false
+    },
 
     // Continue adding guests here...
 ];
